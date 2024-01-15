72b1a722948dc6b413218f399dccda1d22bfc201
ramy <Phrankie@DESKTOP-TL7U6VL.(none)>
2024-01-15 22:27:11 +0200

    add costum appbar to home view

---
+++ lib/features/home/presentation/views/widgets/costum_appbar.dart
@@ -0,0 +1,21 @@
+import 'package:bookly_app/constans.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CostumAppBar extends StatelessWidget {
+  const CostumAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+          padding: const EdgeInsets.only(left: 22,top: 27,right: 16),
+          child: Row(
+            children: [
+              Image.asset(KLogo,height: 60,),
+              const Spacer(),
+              IconButton(onPressed: (){}, icon: const Icon(FontAwesomeIcons.magnifyingGlass,size: 28,))
+            ],
+          ),
+        );
+  }
+}
